179b392c80307753d6f5180bdede2c5402bca8d2
Ramen Kaur <ramendosanjh@outlook.com>
2023-11-04 22:34:18 -0400

    routes index.js commIt

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Wrong route!'));
 
 module.exports = router;
